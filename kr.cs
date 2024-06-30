250021684f0d2b5e67eabefa98d2836298ddcb92
tanushagor <148441937+tanushagor@users.noreply.github.com>
2024-07-01 00:34:05 +0300

    Add files via upload

---
+++ kr.cs
@@ -0,0 +1,45 @@
+string[] CreateArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < size; i += 1)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i += 1)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+string[] ResultArray(string[] array)
+{
+    int c = 0;
+    int j = 0;
+    for (int i = 0; i < array.Length; i += 1)
+    {
+        if (array[i].Length <= 3) c += 1;
+    }
+    string[] arrayRes = new string[c];
+    for (int i = 0; i < array.Length; i += 1)
+     {
+        if (array[i].Length <= 3)
+        {
+            arrayRes[j] = array[i];
+            j += 1;
+        }
+    }
+    return arrayRes;
+}
+string[] arrayFaсt = CreateArray();
+Console.WriteLine();
+Console.WriteLine("Созданный массив:");
+ShowArray(arrayFaсt);
+Console.WriteLine();
+string[] arrayRes = ResultArray(arrayFaсt);
+Console.WriteLine("Новый массив из строк, длина которых меньше, либо равна 3 символам:");
+ShowArray(arrayRes);
